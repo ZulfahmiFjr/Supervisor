8b804c37b70932c5073e9cbd65060c5936736511
ZulfahmiFjr <mccraft874@gmail.com>
2026-01-30 03:11:13 +0700

    remove dependcies chalk

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,41 +1,58 @@
-const chalk = require("chalk");
-
 function twoChar(text) {
-    return (text + '').padStart(2, "0");
+    return (text + "").padStart(2, "0");
 }
 
 function getTime() {
-    return twoChar((new Date()).getHours()) + ":" + twoChar((new Date()).getMinutes()) + ":" + twoChar((new Date()).getSeconds());
+    const d = new Date();
+    return `${twoChar(d.getHours())}:${twoChar(d.getMinutes())}:${twoChar(d.getSeconds())}`;
 }
+
+// warna aktif kalo terminal mendukung dan gak didisable aja
+const COLOR_ENABLED =
+    process.stdout.isTTY &&
+    process.env.TERM !== "dumb" &&
+    !("NO_COLOR" in process.env);
+
+const ansi = (code) => (text) =>
+    COLOR_ENABLED ? `\x1b[${code}m${text}\x1b[0m` : text;
+
+const C = {
+    cyan: ansi(36),
+    gray: ansi(90),
+    white: ansi(97),
+    magenta: ansi(35),
+    yellow: ansi(33),
+    red: ansi(31)
+};
 
 const logger = {
     debugLevel: 0,
 
-    _log: (text, prefix, color) => {
-        console.log(`${chalk.cyan(getTime())} ${color(`[${prefix}]: ${text}`)}`);
+    _log: (text, prefix, colorFn) => {
+        console.log(`${C.cyan(getTime())} ${colorFn(`[${prefix}]: ${text}`)}`);
     },
 
     debug: (text) => {
-        if(logger.debugLevel > 0) {
-            logger._log(text, "DEBUG", chalk.gray);
+        if (logger.debugLevel > 0) {
+            logger._log(text, "DEBUG", C.gray);
         }
     },
-      
+
     info: (text) => {
-        logger._log(text, "INFO", chalk.white);
+        logger._log(text, "INFO", C.white);
     },
 
     notice: (text) => {
-        logger._log(text, "NOTICE", chalk.magenta);
+        logger._log(text, "NOTICE", C.magenta);
     },
 
     warning: (text) => {
-        logger._log(text, "WARNING", chalk.yellow);
+        logger._log(text, "WARNING", C.yellow);
     },
 
     error: (text) => {
-        logger._log(text, "ERROR", chalk.red);
+        logger._log(text, "ERROR", C.red);
     }
-}
+};
 
 module.exports = logger;
